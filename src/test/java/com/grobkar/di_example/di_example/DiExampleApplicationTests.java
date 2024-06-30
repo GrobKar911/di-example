89d783a490248a53c128dfdf4b1f84094eaadfd8
48516 <grobkar91@gmail.com>
2024-06-30 17:05:38 +0200

    Deveploment: Testing Spring context

--- src/test/java/com/grobkar/di_example/di_example/DiExampleApplicationTests.java
+++ src/test/java/com/grobkar/di_example/di_example/DiExampleApplicationTests.java
@@ -1,11 +1,32 @@
 package com.grobkar.di_example.di_example;
 
+import com.grobkar.di_example.di_example.controllers.MyController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class DiExampleApplicationTests {
 
+	@Autowired
+	ApplicationContext applicationContext;
+
+	@Autowired
+	MyController myController;
+
+	@Test
+	void  testAutowireOfController() {
+		System.out.println(myController.sayHello());
+	}
+
+	@Test
+	void testGetControllerFromCtx() {
+
+		MyController myController = applicationContext.getBean(MyController.class);
+
+		System.out.println(myController.sayHello());
+	}
 	@Test
 	void contextLoads() {
 	}
