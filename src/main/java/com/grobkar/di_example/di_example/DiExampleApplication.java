e2a0d16ac13198469711b45b945f688a0170fe02
48516 <grobkar91@gmail.com>
2024-06-30 16:40:54 +0200

    Development: Spring Context review

--- src/main/java/com/grobkar/di_example/di_example/DiExampleApplication.java
+++ src/main/java/com/grobkar/di_example/di_example/DiExampleApplication.java
@@ -1,13 +1,21 @@
 package com.grobkar.di_example.di_example;
 
+import com.grobkar.di_example.di_example.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DiExampleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DiExampleApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(DiExampleApplication.class, args);
+
+		MyController controller = ctx.getBean(MyController.class);
+
+		System.out.println("I'm in the Main Method");
+
+		System.out.println(controller.sayHello());
 	}
 
 }
---
+++ src/main/java/com/grobkar/di_example/di_example/controllers/MyController.java
@@ -0,0 +1,13 @@
+package com.grobkar.di_example.di_example.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    public String sayHello() {
+        System.out.println("I'm in the controller");
+
+        return "Hello Everyone";
+    }
+}
